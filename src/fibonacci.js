e5eccb5f16c0548adef767c466f7d008890e2335
mfullford <marissafullford@gmail.com>
2017-10-11 15:36:13 -0600

    fib

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -6,3 +6,15 @@
      * @returns {Number} the nth element of the Fibonacci sequence.
      */
     // TODO: return definition of `fibonacci` function here
+
+function fibonacci(number) {
+	if (number<0) {
+		return 0;
+	 }
+	if (number === 0 || number === 1) {
+		return 1;
+	}
+	return fibonacci(number-1) + fibonacci(number-2);
+}
+
+module.exports = fibonacci;
